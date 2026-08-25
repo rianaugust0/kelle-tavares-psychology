724d48024344302c68865cc0919166ad30a6601d
Rian Augusto Alves da Silva <rian.silva@softysfalcon.com.br>
2026-08-25 17:31:03 -0300

    feat: update career and formation photo with new uploaded portrait

---
+++ dist/assets/kelle-4-eOQNvbwX.js
@@ -0,0 +1,1 @@
+var e=`/assets/kelle-3-BDQ3HRFj.png`,t=`/assets/kelle-4-CfGmkk0w.png`;export{e as n,t};
--- dist/assets/kelle-4-m-29WAhe.js
+++
@@ -1,1 +0,0 @@
-var e=`/assets/kelle-3-nRasgvQD.png`,t=`/assets/kelle-4-CfGmkk0w.png`;export{e as n,t};
